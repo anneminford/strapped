762c86c601af390b903d57403af30257969d5c6a
Anne Minford <anne.minford@regus.com>
2016-11-11 11:05:02 +0000

    carousel only on home page

--- templates/template-common.php
+++ templates/template-common.php
@@ -10,19 +10,23 @@
  */
 
 get_header(); ?>
-  <?php get_template_part( 'carousel', 'page' );
+  <?php if(is_front_page()) { 
+      get_template_part('carousel','page');
+      echo 'User is on the homepage.';
+    }
   ?>
-
-
+ 
   <?php while ( have_posts() ) : the_post(); ?>
       <?php get_template_part( 'content', 'page' );   
   ?>
-   
   <?php endwhile; ?>
-   <?php if(is_page('booking-availability')) { 
+
+  <?php if(is_page('booking-availability')) { 
 	    get_template_part('page','contact');
 	    echo 'User is on the homepage.';
 	  }
 	?>
 </div >
 <?php get_footer(); ?>
+
+
